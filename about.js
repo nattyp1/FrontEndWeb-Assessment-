22d14cd3e4cb9e42c1b58a1906b952b709658f44
nattyp1 <80240494+nattyp1@users.noreply.github.com>
2021-11-01 18:27:38 -0400

    First Commit

---
+++ about.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	console.log('form submit');
+
+}
+
+test.addEventListener("mouseover", function( event )
+let form = document.querySelector('form#contact');
+
+form.addEventListener('submit', handleSubmit);
